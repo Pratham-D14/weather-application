325be8388a6b4956a26d863111e686394fe93118
pratham.darji <pratham.darji@deptagency.com>
2026-02-07 03:52:49 +0530

    changing the weather api from http - https for secure connection

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -37,7 +37,7 @@ export default function Home() {
       setLoading(true);
 
       const response = await fetch(
-        `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=${API_KEY}`,
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=${API_KEY}`,
       );
 
       if (!response.ok) {
